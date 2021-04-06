becf4d572a34d5a56d36548fab70532ef3ad2e4a
Jackson Wold <jacksonrwold@gmail.com>
2021-04-06 15:17:23 -0700

    Added the pages directory and the actual pages, as well as set up the links. Moved the portfolio container call to the home page

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 import moment from "moment";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import PortfolioContainer from "./portfolio/portfolio-container-hooks";
 import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home"
+import About from "./pages/about"
+import Contact from "./pages/contact"
+import Blog from "./pages/blog"
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <NavigationContainer />
+        <Router>
+          <div>
+            <NavigationContainer />
+            <Switch>
+              <Route exact path="/" component={Home}></Route>
+              <Route path="/about-me" component={About}></Route>
+              <Route path="/contact" component={Contact}></Route>
+              <Route path="/blog" component={Blog}></Route>
+            </Switch>
+          </div>
+        </Router>
+
         <h1>Jackson React Starter</h1>
         <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
         <PortfolioContainer />
--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react"
+import { NavLink } from "react-router-dom"
 
 export default function() {
     const [loggedInStatus, setLoggedInStatus] = useState(true)
     
     return (
         <div>
-            <button>Home</button>
-            <button>About</button>
-            <button>Contact</button>
-            <button>Blog</button>
+            <NavLink exact to="/" activeClassName="nav-link-active">Home</NavLink>
+            <NavLink exact to="/about-me" activeClassName="nav-link-active">About</NavLink>
+            <NavLink exact to="/contact" activeClassName="nav-link-active">Contact</NavLink>
+            <NavLink exact to="/blog" activeClassName="nav-link-active">Blog</NavLink>
             {loggedInStatus == true ? <button>Add Blog</button> : null}
         </div>
     )
